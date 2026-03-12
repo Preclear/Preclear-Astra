fc3c79019f835b2745dd5ce52aa6229bed9c6114
Robel <robelsmac@Robels-MacBook-Air.local>
2026-03-11 22:34:29 -0400

    added supabase file

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './styles/global.css';
+
+function App() {
+  return (
+    <div className="page">
+      <button type="button" className="signup-btn">
+        Sign Up
+      </button>
+    </div>
+  );
+}
+
+export default App;
